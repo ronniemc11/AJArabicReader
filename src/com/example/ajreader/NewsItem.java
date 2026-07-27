76b6da03704b2b38cadf18ad189fa17ef8d4e110
ronniemc11 <ronniemcnutt11@gmail.com>
2026-07-27 00:24:20 -0700

    Create NewsItem.java

---
+++ src/com/example/ajreader/NewsItem.java
@@ -0,0 +1,15 @@
+package com.example.ajreader;
+
+public class NewsItem {
+    public String title;
+    public String description;
+    public String link;
+    public String pubDate;
+
+    public NewsItem(String title, String description, String link, String pubDate) {
+        this.title = title;
+        this.description = description;
+        this.link = link;
+        this.pubDate = pubDate;
+    }
+}
