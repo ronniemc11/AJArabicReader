7f5a3950841568228f7f0d3b4641d53dd8f2da4a
ronniemc11 <ronniemcnutt11@gmail.com>
2026-07-27 23:42:52 -0700

    Update NewsItem.java

--- src/com/example/ajreader/NewsItem.java
+++ src/com/example/ajreader/NewsItem.java
@@ -5,11 +5,13 @@ public class NewsItem {
     public String description;
     public String link;
     public String pubDate;
+    public String imageUrl;
 
-    public NewsItem(String title, String description, String link, String pubDate) {
+    public NewsItem(String title, String description, String link, String pubDate, String imageUrl) {
         this.title = title;
         this.description = description;
         this.link = link;
         this.pubDate = pubDate;
+        this.imageUrl = imageUrl;
     }
 }
